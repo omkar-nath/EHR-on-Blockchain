6d12dab16384732ee8591d11a2b51c405f5123a0
Omkar8039 <Omilpsablock@gmail.com>
2018-04-22 17:38:21 +0530

    Smart contract updated

--- Medic.sol
+++ Medic.sol
@@ -6,46 +6,68 @@ contract Medical
     {
         bytes32 email_p;
         uint adhar_id;
-        bytes32 public_key;
+       
+    }
+    struct Date
+    {
+        uint16 year;
+        uint8 day;
+        uint8 month;
     }
     struct Doctor
     {
         bytes32 email_d;
         uint adhar_id_d;
-       bytes32 public_key;
+        
+        
+       
+       
     }
-  
+    struct IpfsHash
+    {
+        bytes32 first;
+        bytes32 second;
+        bytes32 third;
+    }
+   struct EhrDocument
+   {
+       bytes32 uploadedBy;
+       bytes32 belongsTo;
+       Date dateofupload;
+       IpfsHash encryptedHash;
+       
+   }
     mapping(address=>Patient) public PatientStruct ;
     mapping(address=>Doctor) public DoctorStruct;
     mapping(bytes32=>address) public PatientAddressMap;
-    mapping(bytes32=>bytes32) public PatientPublicKey;
-    mapping(bytes32=>bytes32) public DoctorPublicKey;
+  
+   mapping(address=>EhrDocument) public PatientDocs;
    
     
     mapping(address=>mapping(address=>uint)) patientgrantaccess;
     mapping(bytes32=>bytes32) public usernameEmail;
     mapping(bytes32=>address) public DoctorAddressMap;
-    function setPatient(bytes32 email_id,uint adhar_id,bytes32 username,bytes32 public_key) public returns(bool success)
+    function setPatient(bytes32 email_id,uint adhar_id,bytes32 username) public returns(bool success)
     {
         PatientStruct[msg.sender].email_p=email_id;
         PatientStruct[msg.sender].adhar_id=adhar_id;
         PatientAddressMap[keccak256(username)]=msg.sender;
-        PatientStruct[msg.sender].public_key=public_key;
+       
         usernameEmail[keccak256(username)]=email_id;
-        PatientPublicKey[username]=public_key;
+     
         return true;
     }
     function getPatient(address patient_add) public constant returns(bytes32 email_p,uint adhar_id)
     {
         return (PatientStruct[patient_add].email_p,PatientStruct[patient_add].adhar_id);
     }
-    function setDoctor(bytes32 email_id,uint adhar_id,bytes32 username,bytes32 public_key) public returns(bool success)
+    function setDoctor(bytes32 email_id,uint adhar_id,bytes32 username) public returns(bool success)
     {
         DoctorStruct[msg.sender].email_d=email_id;
         DoctorStruct[msg.sender].adhar_id_d=adhar_id;
         DoctorAddressMap[keccak256(username)]=msg.sender;
-        DoctorStruct[msg.sender].public_key=public_key;
-        DoctorPublicKey[username]=public_key;
+       
+   
         
         return true;
     }
@@ -87,16 +109,23 @@ contract Medical
         else
         return false;
     }
-    function getPatientPublicKey(bytes32 username) public constant returns(bytes32)
+   
+    function storeIpfs(bytes32 doc_username,bytes32 patient_username,uint16 year,uint8 day,uint8 month,bytes32 first,bytes32 second,bytes32 third) public returns(bool success)
     {
-        return PatientPublicKey[username];
+        PatientDocs[msg.sender].uploadedBy=doc_username;
+        PatientDocs[msg.sender].belongsTo=patient_username;
+        PatientDocs[msg.sender].encryptedHash.first=first;
+        PatientDocs[msg.sender].encryptedHash.second=second;
+        PatientDocs[msg.sender].encryptedHash.third=third;
+        PatientDocs[msg.sender].dateofupload.day=day;
+        PatientDocs[msg.sender].dateofupload.month=month;
+        PatientDocs[msg.sender].dateofupload.year=year;
+        return true;
     }
-    function getDoctorPublicKey(bytes32 username) public constant returns(bytes32)
-    {
-        return DoctorPublicKey[username];
-    }
-  
-  
+  function getEncryptedHash(address patient_address) public constant returns(bytes32,bytes32,bytes32)
+  {
+      return (PatientDocs[patient_address].encryptedHash.first,PatientDocs[patient_address].encryptedHash.second,PatientDocs[patient_address].encryptedHash.third);
+  }
 
     
    
